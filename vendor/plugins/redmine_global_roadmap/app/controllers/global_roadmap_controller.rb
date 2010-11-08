92a780b2fe180dafec5f4c5d4f86496cf7c5104b
Gordon B. Isnor <gordon@gbi.(none)>
2010-11-08 14:47:05 -0500

    global roadmap active project list

--- vendor/plugins/redmine_global_roadmap/app/controllers/global_roadmap_controller.rb
+++ vendor/plugins/redmine_global_roadmap/app/controllers/global_roadmap_controller.rb
@@ -4,7 +4,16 @@ class GlobalRoadmapController < ApplicationController
   def index
     @versions = Version.find(:all, :include => [:project, :custom_values], :order => "versions.effective_date ASC")
     @versions.reject! { |v| v.completed? }
-    @versions = @versions.paginate(:page => params[:page],:per_page=>10)
+    @versions = @versions.paginate(:page => params[:page], :per_page => 10)
+    @active_projects = Project.find(
+      :all, 
+      :order => "name ASC", 
+      :joins => { :issues => :status },
+      :conditions => ["#{IssueStatus.table_name}.is_closed = ? AND issues.tracker_id=5", false],
+      :group => "projects.id",
+      :select => "projects.*"
+    )
+    
   end
     
 end
