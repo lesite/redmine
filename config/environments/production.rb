2540204f9f70ca0dacac555e6dc9eaf1051ae8f0
gordonbisnor <isnorcreative@gmail.com>
2011-03-30 13:58:28 -0400

    updates

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,3 +28,5 @@ config.action_mailer.raise_delivery_errors = false
 
 # No email in production log
 config.action_mailer.logger = nil
+
+ExceptionNotification::Notifier.exception_recipients = %w(mat@lesite.ca christopher@lesite.ca)
