de6c25c4e2482a615dc918557d3e6631fecced8f
Gordon B. Isnor <gordonbisnor@gmail.com>
2011-06-13 21:53:36 -0400

    updates

---
+++ vendor/plugins/redmine_lesite_custom_fields_form/init.rb
@@ -0,0 +1,10 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_lesite_custom_fields_form do
+  name 'Redmine LeSite Custom Fields form plugin'
+  author 'Gordon B. Isnor'
+  description 'Add admin checkbox'
+  version '0.0.1'
+  url 'http://www.isnorcreative.com'
+  author_url 'http://www.isnorcreative.com'
+end
---
+++ vendor/plugins/redmine_lesite_issues_form_custom_fields/init.rb
@@ -0,0 +1,10 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_lesite_issues_form_custom_fields do
+  name 'Redmine LeSite Issues Form Custom Fields plugin'
+  author 'Gordon B. Isnor'
+  description 'Add admin clause'
+  version '0.0.1'
+  url 'http://www.isnorcreative.com'
+  author_url 'http://www.isnorcreative.com'
+end
